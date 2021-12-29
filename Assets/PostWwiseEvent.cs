29d9dcb71934613dd0bd9b1a248d331a6ee515d5
Nasinera <ronny.tgkt@gmail.com>
2021-12-29 17:19:56 +0100

    Schritt angepasst

--- Assets/PostWwiseEvent.cs
+++ Assets/PostWwiseEvent.cs
@@ -8,7 +8,7 @@ public class PostWwiseEvent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TestEvent.Post(gameObject);    
+        //TestEvent.Post(gameObject);    
     }
 
     public void SchrittSound()
