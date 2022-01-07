29cfb5f741f89764ee2daf97bfad869800d6656b
Nasinera <ronny.tgkt@gmail.com>
2022-01-07 20:23:46 +0100

    Finish (F*** Unity)

--- Assets/PlayerInstructions.cs
+++ Assets/PlayerInstructions.cs
@@ -20,19 +20,19 @@ public class PlayerInstructions : MonoBehaviour
     {
         if (GameObject.Find("ETObj") == null && TextChanged1 == false) //if erstes WM erledigt, nächste Instruction
         {
-            Instructions = "Whats that on my Desk?";
+            Instructions = "Whats that on my desk?";
             PlayerInstructionsElement.text = Instructions;
             TextChanged1 = true;
         }
         if (GameObject.Find("ETObj") == null && GameObject.Find("ETObj2") == null && TextChanged2 == false) //If Esstisch und Coffeetable WM erldigt, letzte Instruction
         {
-            Instructions = "Didnt I see a third newspaper in the shelf?";
+            Instructions = "Didn't I see a third newspaper in the shelf?";
             PlayerInstructionsElement.text = Instructions;
             TextChanged2 = true;
         }
         if (GameObject.Find("ETObj") == null && GameObject.Find("ETObj2") == null && GameObject.Find("ETObj3") == null && TextChanged3 == false) //If alle Wms erledigt, Letzter Text
         {
-            Instructions = "So thats what happened. But is it really my fault?";
+            Instructions = "So thats what happened.. I need fresh air..";
             PlayerInstructionsElement.text = Instructions;
             TextChanged3 = true;
         }
